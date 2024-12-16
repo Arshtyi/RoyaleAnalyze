6dc965d967855d7dc00d9fbf039eff52a94a2148
Ar_T <640006128.qq.com>
2024-12-16 15:12:29 +0800

    small change

--- src/menu.py
+++ src/menu.py
@@ -1,10 +1,11 @@
 """
-menu
+creat the menu and judge choice
 """
 def creat_menu():
     print("选择操作类型:")
     print("1.查询部落战贡献")
     print("请确保删除输出文件以免发生格式错误")
+
 
 def Get_choices():
     choice = (int)(input())
